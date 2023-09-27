13dc48c1222138dac59d4854dde1468defff440b
chapman-blockchain <limth1312@naver.com>
2023-09-27 10:53:52 +0900

    express default setting2

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 const express = require("express");
 const app = express();
 const nunjucks = require("nunjucks");
-const router = require("");
+// const router = require("./src/index");
+
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+});
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+// app.use(router);
+
+app.listen(3000, () => {
+  console.log("Server Start");
+});
---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const boardRouter = require("./board/board.route");
+const userRouter = require("./user/user.route");
+
+router.get("/", (req, res) => {
+  res.render("index.html");
+  res.render("index.html", { user: req.user });
+});
+
+router.use("/boards", boardRouter);
+router.use("/users", userRouter);
+
+module.exports = router;
