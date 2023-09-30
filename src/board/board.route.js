303a9be2ee76ce1a3262b86056ba64d1491cdfc0
seon-blockchain <flowermansk@naver.com>
2023-10-01 03:02:02 +0900

    board4 announcement, freeboard finished

--- src/board/board.route.js
+++ src/board/board.route.js
@@ -2,13 +2,35 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./board.controller");
 
-router.get("/list", controller.getList);
-router.get("/view", controller.getView);
-router.get("/write", controller.getWrite);
-router.get("/modify", controller.getModify);
+router.get("/freeboard/list", controller.freeboardGetList);
+router.get("/freeboard/view", controller.freeboardGetView);
+router.get("/freeboard/write", controller.freeboardGetWrite);
+router.get("/freeboard/modify", controller.freeboardGetModify);
+router.post("/freeboard/write", controller.freeboardPostWrite);
+router.post("/freeboard/modify", controller.freeboardPostModify);
+router.post("/freeboard/delete", controller.freeboardPostDelete);
 
-router.post("/write", controller.postWrite);
-router.post("/modify", controller.postModify);
-router.post("/delete", controller.postDelete);
+router.get("/announcement/list", controller.announcementGetList);
+router.get("/announcement/view", controller.announcementGetView);
+router.get("/announcement/write", controller.announcementGetWrite);
+router.get("/announcement/modify", controller.announcementGetModify);
+router.post("/announcement/write", controller.announcementPostWrite);
+router.post("/announcement/modify", controller.announcementPostModify);
+router.post("/announcement/delete", controller.announcementPostDelete);
+
+module.exports = router;
+
+// const express = require("express");
+// const router = express.Router();
+// const controller = require("./board.controller");
+
+// router.get("/list", controller.getList);
+// router.get("/view", controller.getView);
+// router.get("/write", controller.getWrite);
+// router.get("/modify", controller.getModify);
+
+// router.post("/write", controller.postWrite);
+// router.post("/modify", controller.postModify);
+// router.post("/delete", controller.postDelete);
 
 module.exports = router;
--- src/board/board.service.js
+++ src/board/board.service.js
@@ -1,58 +1,192 @@
-const boardRepository = require("./board.repository");
+// const boardRepository = require("./board.repository");
 
-exports.getFindAll = async () => {
-    try {
-        const result = await boardRepository.findAll();
-        return result;
-    } catch (e) {
-        throw new Error(`Service 오류 발생 ${e.message}`);
+const { FreeBoardRepository, AnnouncementBoardRepository } = require("./board.repository");
+
+const freeboardRepo = new FreeBoardRepository();
+const announcementboardRepo = new AnnouncementBoardRepository();
+
+class FreeBoardService {
+    constructor() {
+        this.repository = new FreeBoardRepository();
     }
+
+    async getFindAll() {
+        try {
+            const result = await this.repository.findAll();
+            return result;
+        } catch (e) {
+            throw new Error(`Service 오류 발생 ${e.message}`);
+        }
+    }
+
+    async getFindOne(id) {
+        try {
+            await this.repository.incrementHit(id);
+            const result = await this.repository.findOne(id);
+            return result;
+        } catch (e) {
+            throw new Error(`Service 오류 발생 ${e.message}`);
+        }
+    }
+
+    async getFindOneWithoutIncreamentHit(id) {
+        try {
+            const result = await this.repository.findOne(id);
+            return result;
+        } catch (e) {
+            throw new Error(`Service 오류 발생 ${e.message}`);
+        }
+    }
+
+    async write(data) {
+        try {
+            const result = await this.repository.write(data);
+            return result;
+        } catch (e) {
+            throw new Error(`Service 오류 발생 ${e.message}`);
+        }
+    }
+
+    async modify(id, data) {
+        try {
+            const result = await this.repository.modify(id, data);
+            return result;
+        } catch (e) {
+            throw new Error(`Service 오류 발생 ${e.message}`);
+        }
+    }
+
+    async delete(id) {
+        try {
+            const result = await this.repository.delete(id);
+            return result;
+        } catch (e) {
+            throw new Error(`Service 오류 발생 ${e.message}`);
+        }
+    }
+}
+
+class AnnouncementBoardService {
+    constructor() {
+        this.repository = new AnnouncementBoardRepository();
+    }
+
+    async getFindAll() {
+        try {
+            const result = await this.repository.findAll();
+            return result;
+        } catch (e) {
+            throw new Error(`Service 오류 발생 ${e.message}`);
+        }
+    }
+
+    async getFindOne(id) {
+        try {
+            await this.repository.incrementHit(id);
+            const result = await this.repository.findOne(id);
+            return result;
+        } catch (e) {
+            throw new Error(`Service 오류 발생 ${e.message}`);
+        }
+    }
+
+    async getFindOneWithoutIncreamentHit(id) {
+        try {
+            const result = await this.repository.findOne(id);
+            return result;
+        } catch (e) {
+            throw new Error(`Service 오류 발생 ${e.message}`);
+        }
+    }
+
+    async write(data) {
+        try {
+            const result = await this.repository.write(data);
+            return result;
+        } catch (e) {
+            throw new Error(`Service 오류 발생 ${e.message}`);
+        }
+    }
+
+    async modify(id, data) {
+        try {
+            const result = await this.repository.modify(id, data);
+            return result;
+        } catch (e) {
+            throw new Error(`Service 오류 발생 ${e.message}`);
+        }
+    }
+
+    async delete(id) {
+        try {
+            const result = await this.repository.delete(id);
+            return result;
+        } catch (e) {
+            throw new Error(`Service 오류 발생 ${e.message}`);
+        }
+    }
+}
+
+module.exports = {
+    FreeBoardService,
+    AnnouncementBoardService,
 };
 
-exports.getFindOne = async (id) => {
-    try {
-        await boardRepository.incrementHit(id);
-        const result = await boardRepository.findOne(id);
-        // console.log(result);
-        return result;
-    } catch (e) {
-        throw new Error(`Service 오류 발생 ${e.message}`);
-    }
-};
+// ---------------- 클래스화 이전의 코드들
 
-exports.getFindOneWithoutIncreamentHit = async (id) => {
-    try {
-        const result = await boardRepository.findOne(id);
-        return result;
-    } catch (e) {
-        throw new Error(`Service 오류 발생 ${e.message}`);
-    }
-};
+// exports.getFindAll = async () => {
+//     try {
+//         const result = await boardRepository.findAll();
+//         return result;
+//     } catch (e) {
+//         throw new Error(`Service 오류 발생 ${e.message}`);
+//     }
+// };
 
-exports.write = async (data) => {
-    try {
-        const result = await boardRepository.write(data);
-        // console.log("Service Layer에서 반환된 ID 값:", result.id);
-        return result;
-    } catch (e) {
-        throw new Error(`Service 오류 발생 ${e.message}`);
-    }
-};
+// exports.getFindOne = async (id) => {
+//     try {
+//         await boardRepository.incrementHit(id);
+//         const result = await boardRepository.findOne(id);
+//         // console.log(result);
+//         return result;
+//     } catch (e) {
+//         throw new Error(`Service 오류 발생 ${e.message}`);
+//     }
+// };
 
-exports.modify = async (id, data) => {
-    try {
-        const result = await boardRepository.modify(id, data);
-        return result;
-    } catch (e) {
-        throw new Error(`Service 오류 발생 ${e.message}`);
-    }
-};
+// exports.getFindOneWithoutIncreamentHit = async (id) => {
+//     try {
+//         const result = await boardRepository.findOne(id);
+//         return result;
+//     } catch (e) {
+//         throw new Error(`Service 오류 발생 ${e.message}`);
+//     }
+// };
 
-exports.delete = async (id) => {
-    try {
-        const result = await boardRepository.delete(id);
-        return result;
-    } catch (e) {
-        throw new Error(`Service 오류 발생 ${e.message}`);
-    }
-};
+// exports.write = async (data) => {
+//     try {
+//         const result = await boardRepository.write(data);
+//         // console.log("Service Layer에서 반환된 ID 값:", result.id);
+//         return result;
+//     } catch (e) {
+//         throw new Error(`Service 오류 발생 ${e.message}`);
+//     }
+// };
+
+// exports.modify = async (id, data) => {
+//     try {
+//         const result = await boardRepository.modify(id, data);
+//         return result;
+//     } catch (e) {
+//         throw new Error(`Service 오류 발생 ${e.message}`);
+//     }
+// };
+
+// exports.delete = async (id) => {
+//     try {
+//         const result = await boardRepository.delete(id);
+//         return result;
+//     } catch (e) {
+//         throw new Error(`Service 오류 발생 ${e.message}`);
+//     }
+// };
