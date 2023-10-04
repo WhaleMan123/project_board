a47080ad4ea6411105e5d2863d77e238854eb09a
chapman-blockchain <limth1312@naver.com>
2023-10-04 12:03:41 +0900

    2231004_12:03_login_5

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -3,8 +3,13 @@ const userService = require("./user.service");
 exports.getLogin = (req, res) => {
   res.render("login.html");
 };
-exports.postLogin = (req, res) => {
-  const result = userService.postLogin();
-  console.log(result);
-  res.redirect("/");
+exports.postLogin = async (req, res, next) => {
+  try {
+    const result = await userService.postLogin();
+    console.log(result);
+    res.redirect("/");
+  } catch (error) {
+    // throw new Error("Controller Error " + error.message);
+    next();
+  }
 };
--- src/user/user.service.js
+++ src/user/user.service.js
@@ -1,6 +1,13 @@
 const userRepository = require("./user.repository");
 
-exports.postLogin = () => {
-  const result = userRepository.findOneByUserInfo("web7722", "1234");
-  return result;
+exports.postLogin = async () => {
+  try {
+    const result = await userRepository.findOneByUserInfo(
+      "web7722@gmail.com",
+      "1234"
+    );
+    return result;
+  } catch (error) {
+    throw new Error("Service Error " + error.message);
+  }
 };
